12f6e34552afbc3e65c6a70413042eff0d9811a1
wiwi20 <jiahui123123@hotmail.com>
2022-09-12 13:03:04 +0200

    Added jump button

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,38 +7,40 @@ public class PlayerMovement : MonoBehaviour
 
     public Joystick joystick;
 
-    private float horizontal;
-    private float speed = 8f;
-    private float jumpingPower = 16f;
-    private bool isFacingRight = true;
+    public float horizontal;
+    public float speed = 8f;
+    public float jumpingPower = 16f;
+    public bool isFacingRight = true;
 
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] public Rigidbody2D rb;
+    [SerializeField] public Transform groundCheck;
+    [SerializeField] public LayerMask groundLayer;
 
     // Update is called once per frame
     void Update()
     {
         horizontal = joystick.Horizontal;
         horizontal = Input.GetAxisRaw("Horizontal");
-        if(Input.GetButtonDown("Jump") && IsGrounded()){
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-        }
-        if(Input.GetButtonUp("Jump") && rb.velocity.y > 0f){
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-        }
         Flip();
     }
+    public void jumpButton() {
+        if(IsGrounded()){
+        rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+        if( rb.velocity.y > 0f){
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
 
-    private void FixedUpdate() {
+    public void FixedUpdate() {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
 
-    private bool IsGrounded(){
+    public bool IsGrounded(){
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 
-    private void Flip(){
+    public void Flip(){
         if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f){
             isFacingRight = !isFacingRight;
             Vector3 localScale = transform.localScale;
@@ -47,3 +49,5 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 }
+
+
